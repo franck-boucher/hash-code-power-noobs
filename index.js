b32dd4f584de0058b046028f33c6cbda335a0d59
Nicoletti Sébastien <sebastien.nicoletti@inotekk.com>
2018-03-01 14:08:40 +0100

    example to read and write files

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('coucou');
+const fs = require('fs');
+
+//Write file
+fs.writeFile('file.txt', 'coucou', err => {
+  if (err) {
+    throw err;
+  }
+});
+
+fs.readFile('./exemple-pizza/small.in', (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log(data);
+});
