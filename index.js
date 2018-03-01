88b282f9b9d6970927d4bf62d5ab23b527ad99ba
f.boucher <f.boucher@sictiam.fr>
2018-03-01 18:30:59 +0100

    wip

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const fs = require("fs");
 
-// Write file
-fs.writeFile("./exemple-pizza/file.txt", "coucou", err => {
-  if (err) {
-    throw err;
-  }
-});
+function writeFile(filename, content) {
+  fs.writeFile(filename, content, err => {
+    if (err) {
+      console.log("writeFile error: " + err);
+      throw err;
+    }
+  });
+}
 
-fs.readFile("./exemple-pizza/big.in", (err, data) => {
-  if (err) {
-    throw err;
-  }
+function readFile(filename) {
+  return new Promise((resolve, reject) => {
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      console.log("readFile error: " + err);
+      throw err;
+    }
+    return new Promise(data.toString("utf8");
+  });
+  })
+}
 
-  console.log(data.toString("utf8"));
-});
+writeFile("./exemple-pizza/file.txt", "coucou");
+const content = readFile("./exemple-pizza/big.in");
+console.log(content);
