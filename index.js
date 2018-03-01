4d6a0e0e1d7a64c2cf5eb330013645f8db4f9945
Nicoletti Sébastien <sebastien.nicoletti@inotekk.com>
2018-03-01 19:09:11 +0100

    create FileManager

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-const fs = require("fs");
+const fs = require('fs');
+const FileManager = require('./file-manager');
 
-function writeFile(filename, content) {
-  fs.writeFile(filename, content, err => {
-    if (err) {
-      console.log("writeFile error: " + err);
-      throw err;
-    }
+const path = 'file.txt';
+FileManager.write(path, 'test !').then(() => {
+  FileManager.read(path).then(data => {
+    console.log(data);
   });
-}
-
-function readFile(filename) {
-  return new Promise((resolve, reject) => {
-  fs.readFile(filename, (err, data) => {
-    if (err) {
-      console.log("readFile error: " + err);
-      throw err;
-    }
-    return new Promise(data.toString("utf8");
-  });
-  })
-}
-
-writeFile("./exemple-pizza/file.txt", "coucou");
-const content = readFile("./exemple-pizza/big.in");
-console.log(content);
+});
